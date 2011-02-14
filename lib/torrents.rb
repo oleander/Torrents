0c55d16e6db9840d07279648fca67772231b9766
Linus Oleander <linus@oleander.nu>
2011-02-15 00:20:16 +0100

    Makes it possible to add a page

--- lib/torrents.rb
+++ lib/torrents.rb
@@ -1,17 +1,54 @@
 require 'yaml'
 class Torrents
+  attr_accessor :page
+  
   def initialize
     @trackers = YAML::load(File.read('lib/torrents/trackers.yaml'))
+  end
+  
+  # Set the default page
+  def inner_page
+    (@page ||= @current["start_page_index"]).to_s
+  end
+  
+  def url
+    if @search_value
+      pend = @current["search"].gsub('<SEARCH>', @search_value).gsub('<PAGE>', self.inner_page)
+    else 
+      pend = @current["recent"].gsub('<PAGE>', self.inner_page)
+    end
+    
+    @current["url"] + pend
   end
   
   # Does the trackers exists in the trackers file?
   def exists?(site)
     ! @trackers[site.to_s].nil?
   end
-
-  def self.method_missing(method, *args, &block) 
+  
+  # Makes this the {tracker} tracker
+  def add(tracker)
+    @current = @trackers[tracker.to_s]
+    return self
+  end
+  
+  def page(value)
+    @page = value
+    return self
+  end
+  
+  # Set the search value
+  def search(value)
+    @search_value = value
+    return self
+  end
+  
+  def self.method_missing(method, *args, &block)
     this = Torrents.new
     # Raises an exception if the site isn't in the trackers.yaml file
     raise Exception.new("The site #{method} does not exist") unless this.exists?(method)
+    
+    # Yes, I like return :)
+    return this.add(method)
   end
 end
--- spec/torrents_spec.rb
+++ spec/torrents_spec.rb
@@ -9,4 +9,12 @@ describe Torrents do
       Torrents.random_site
     }.should raise_error(Exception)
   end
+  
+  it "should be possible to add a search string" do
+    Torrents.the_pirate_bay.search("string").url.should eq("http://thepiratebay.org/search/string/0/99")
+  end
+  
+  it "should be possible to specify a page" do
+    Torrents.the_pirate_bay.page(5).url.should eq("http://thepiratebay.org/recent/5")
+  end
 end

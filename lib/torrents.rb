89443a94d1d92742286bd960a920a1ef95ec2ab1
Linus Oleander <linus@oleander.nu>
2011-02-14 23:46:06 +0100

    Making the torrents class very dynamic

--- lib/torrents.rb
+++ lib/torrents.rb
@@ -1,5 +1,17 @@
+require 'yaml'
 class Torrents
-  def self.method_missing(m, *args, &block) 
-    
+  def initialize
+    @trackers = YAML::load(File.read('lib/torrents/trackers.yaml'))
+  end
+  
+  # Does the trackers exists in the trackers file?
+  def exists?(site)
+    ! @trackers[site.to_s].nil?
+  end
+
+  def self.method_missing(method, *args, &block) 
+    this = Torrents.new
+    # Raises an exception if the site isn't in the trackers.yaml file
+    raise Exception.new("The site #{method} does not exist") unless this.exists?(method)
   end
 end
--- spec/torrents_spec.rb
+++ spec/torrents_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 describe Torrents do
-  it "should work" do
+  it "should only respond to that exists in the trackers yaml file" do
+    lambda {
+      Torrents.the_pirate_bay
+    }.should_not raise_error(Exception)
     
+    lambda {
+      Torrents.random_site
+    }.should raise_error(Exception)
   end
 end

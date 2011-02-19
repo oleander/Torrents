b12cdeb28d857f571de29f332f1d50f8194df563
Linus Oleander <linus@oleander.nu>
2011-02-19 01:22:24 +0100

    Testing the download method in the shared class

--- spec/shared_spec.rb
+++ spec/shared_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Container::Shared do
-  it "should work" do
-    
+  before(:each) do
+    @shared = Container::Shared.new
+  end
+  it "should return an empty string when trying to download an empty URL" do
+    @shared.download("non_working_url").should be_empty
+  end
+  
+  it "should return the content of the site if called with the right url" do
+    RestClient.should_receive(:get).with("http://example.com", {:timeout => 10}).exactly(1).times.and_return("123")
+    @shared.download("http://example.com").should eq("123")
   end
 end

792e57e7882bcb1ce47f59d9ac573c95cba22d4f
Linus Oleander <linus@oleander.nu>
2011-02-20 01:11:11 +0100

    Ignoring the cookies files

--- spec/trackers/torrentleech.rb
+++ spec/trackers/torrentleech.rb
@@ -12,34 +12,34 @@ describe Trackers::ThePirateBay do
     })
   end
   
-  it "should only list torrents with the right title" do
-    rest_client("http://thepiratebay.org/search/chuck/0/99/0", "search")
-    torrents = Torrents.the_pirate_bay.search("chuck")
-    
-    torrents.results.each do |torrent|
-      torrent.title.should match(/chuck/i)
-      torrent.title.should_not eq(torrent.torrent)
-    end
-    
-    torrents.should have(30).results
-  end
-  
-  it "should be possible to parse the details view" do
-    rest_client("http://thepiratebay.org/torrent/6173093/", "details")
-    torrent = create_torrent
-    
-    torrent.should be_valid    
-    torrent.seeders.should eq(9383)
-    torrent.should_not be_dead
-  end
-  
-  it "should be possible to list recent torrents" do
-    rest_client("http://thepiratebay.org/recent/5", "recent")
-    torrents = Torrents.the_pirate_bay.page(5).should have(30).results
-  end
-  
-  it "should found 30 recent movies" do
-    rest_client("http://thepiratebay.org/browse/201/5/3", "movies")
-    torrents = Torrents.the_pirate_bay.page(5).category(:movies).should have(30).results
-  end
+  # it "should only list torrents with the right title" do
+  #   rest_client("http://thepiratebay.org/search/chuck/0/99/0", "search")
+  #   torrents = Torrents.the_pirate_bay.search("chuck")
+  #   
+  #   torrents.results.each do |torrent|
+  #     torrent.title.should match(/chuck/i)
+  #     torrent.title.should_not eq(torrent.torrent)
+  #   end
+  #   
+  #   torrents.should have(30).results
+  # end
+  # 
+  # it "should be possible to parse the details view" do
+  #   rest_client("http://thepiratebay.org/torrent/6173093/", "details")
+  #   torrent = create_torrent
+  #   
+  #   torrent.should be_valid    
+  #   torrent.seeders.should eq(9383)
+  #   torrent.should_not be_dead
+  # end
+  # 
+  # it "should be possible to list recent torrents" do
+  #   rest_client("http://thepiratebay.org/recent/5", "recent")
+  #   torrents = Torrents.the_pirate_bay.page(5).should have(30).results
+  # end
+  # 
+  # it "should found 30 recent movies" do
+  #   rest_client("http://thepiratebay.org/browse/201/5/3", "movies")
+  #   torrents = Torrents.the_pirate_bay.page(5).category(:movies).should have(30).results
+  # end
 end
